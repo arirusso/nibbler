6b8de2b28dbc4f17349a755d90d4031bf8762a4f
Ari Russo <ari.russo@gmail.com>
2011-05-01 03:39:05 -0400

    tests

--- test/test_simple_messages.rb
+++ test/test_simple_messages.rb
@@ -6,5 +6,23 @@ class SimpleMessageTest < Test::Unit::TestCase
 
   include Nibbler
   include TestHelper
+ 
+  def test_note_off
+    nibbler = Nibbler.new
+    msg = nibbler.parse(0x80, 0x40, 0x40)
+    assert_equal(msg.class, MIDIMessage::NoteOff)
+    assert_equal(msg.channel, 0)
+    assert_equal(msg.note, 0x40)
+    assert_equal(msg.velocity, 0x40)  
+  end
+  
+  def test_note_on
+    nibbler = Nibbler.new
+    msg = nibbler.parse(0x90, 0x40, 0x40)
+    assert_equal(msg.class, MIDIMessage::NoteOn)
+    assert_equal(msg.channel, 0)
+    assert_equal(msg.note, 0x40)
+    assert_equal(msg.velocity, 0x40)  
+  end  
   
 end
