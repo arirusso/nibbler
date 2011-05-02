109fc0652092c9c572b32fdce6db3829fd0c10d8
Ari Russo <ari.russo@gmail.com>
2011-05-02 00:45:27 -0400

    midilib compat

--- lib/nibbler/midilib_factory.rb
+++ lib/nibbler/midilib_factory.rb
@@ -5,10 +5,52 @@ module Nibbler
   # factory for constructing messages with {midilib}(https://github.com/jimm/midilib)
   # midilib is copyright © 2003-2010 Jim Menard
   class MidilibFactory
+    
+    include MIDI
   
     def initialize
       require 'midilib'
     end
+    
+    def note_off(second_nibble, data_byte_1, data_byte_2)
+      NoteOff.new(second_nibble, data_byte_1, data_byte_2)
+    end
+    
+    def note_on(second_nibble, data_byte_1, data_byte_2)
+      NoteOn.new(second_nibble, data_byte_1, data_byte_2)
+    end
+    
+    def polyphonic_aftertouch(second_nibble, data_byte_1, data_byte_2)
+      PolyPressure.new(second_nibble, data_byte_1, data_byte_2)
+    end
+      
+    def control_change(second_nibble, data_byte_1, data_byte_2)
+      Controller.new(second_nibble, data_byte_1, data_byte_2)
+    end
+    
+    def program_change(second_nibble, data_byte)
+      ProgramChange.new(second_nibble, data_byte)
+    end
+    
+    def channel_aftertouch(second_nibble, data_byte)
+      ChannelPressure.new(second_nibble, data_byte)
+    end
+    
+    def pitch_bend(second_nibble, data_byte_1, data_byte_2)
+      PitchBend.new(second_nibble, data_byte_1, data_byte_2)
+    end
+    
+    def system_exclusive(*a)
+      SystemExclusive.new(*a)
+    end
+    
+    def system_common(second_nibble, data_byte_1, data_byte_2)
+      SystemCommon.new(second_nibble, data_byte_1, data_byte_2)
+    end
+    
+    def system_realtime(second_nibble)
+      SystemRealtime.new(second_nibble)
+    end  
     
   end
 
---
+++ test/test_short_messages_midilib.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/test_helper'
+
+class ShortMessageMidilibTest < Test::Unit::TestCase
+
+  include Nibbler
+  include TestHelper
+ 
+  def test_note_off
+    nibbler = Nibbler.new(:message_lib => :midilib)
+    msg = nibbler.parse(0x80, 0x40, 0x40)
+    assert_equal(MIDI::NoteOff, msg.class)
+    assert_equal(0, msg.channel)
+    assert_equal(0x40, msg.note)
+    assert_equal(0x40, msg.velocity)  
+  end
+  
+  def test_note_on
+    nibbler = Nibbler.new(:message_lib => :midilib)
+    msg = nibbler.parse(0x90, 0x40, 0x40)
+    assert_equal(MIDI::NoteOn, msg.class)
+    assert_equal(0, msg.channel)
+    assert_equal(0x40, msg.note)
+    assert_equal(0x40, msg.velocity)  
+  end  
+  
+  def test_polyphonic_aftertouch
+    nibbler = Nibbler.new(:message_lib => :midilib)
+    msg = nibbler.parse(0xA1, 0x40, 0x40)
+    assert_equal(MIDI::PolyPressure, msg.class)
+    assert_equal(1, msg.channel)
+    assert_equal(0x40, msg.note)
+    assert_equal(0x40, msg.pressure)      
+  end  
+  
+  def test_control_change
+    nibbler = Nibbler.new(:message_lib => :midilib)
+    msg = nibbler.parse(0xB2, 0x20, 0x20)
+    assert_equal(MIDI::Controller, msg.class)
+    assert_equal(msg.channel, 2)
+    assert_equal(0x20, msg.controller)
+    assert_equal(0x20, msg.value)    
+  end
+  
+  def test_program_change
+    nibbler = Nibbler.new(:message_lib => :midilib)
+    msg = nibbler.parse(0xC3, 0x40)
+    assert_equal(MIDI::ProgramChange, msg.class)
+    assert_equal(3, msg.channel)
+    assert_equal(0x40, msg.program)  
+  end
+  
+  def test_channel_aftertouch
+    nibbler = Nibbler.new(:message_lib => :midilib)
+    msg = nibbler.parse(0xD3, 0x50)
+    assert_equal(MIDI::ChannelPressure, msg.class)
+    assert_equal(3, msg.channel)
+    assert_equal(0x50, msg.pressure)  
+  end  
+    
+  def test_pitch_bend
+    # to-do handle the midilib lsb/msb
+    # right now the second data byte is being thrown away
+    nibbler = Nibbler.new(:message_lib => :midilib)
+    msg = nibbler.parse(0xE0, 0x20, 0x00)
+    assert_equal(MIDI::PitchBend, msg.class)
+    assert_equal(0, msg.channel)
+    assert_equal(0x20, msg.value) 
+  end   
+   
+end
