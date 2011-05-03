4aae66e71ca16a6faef640de5e457092cf09d264
Ari Russo <ari.russo@gmail.com>
2011-05-02 21:17:13 -0400

    test message factory

--- test/test_midi_message_factory.rb
+++ test/test_midi_message_factory.rb
@@ -67,6 +67,34 @@ class MIDIMessageFactoryTest < Test::Unit::TestCase
     assert_equal(0x20, msg.low)
     assert_equal(0x00, msg.high)  
   end   
+  
+  def test_system_exclusive_command
+    factory = MIDIMessageFactory.new
+    msg = factory.system_exclusive(0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7)   
+    assert_equal(MIDIMessage::SystemExclusive::Command, msg.class)
+    assert_equal([0xF0, 0x41, 0x10, 0x42, 0x12, [0x40, 0x00, 0x7F], [0x00], 0x41, 0xF7], msg.to_a)
+    assert_equal([0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7], msg.to_bytes)
+    assert_equal("F04110421240007F0041F7", msg.to_hex_s)
+  end
+  
+  def test_system_exclusive_request
+    factory = MIDIMessageFactory.new
+    msg = factory.system_exclusive(0xF0, 0x41, 0x10, 0x42, 0x11, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7)   
+    assert_equal(MIDIMessage::SystemExclusive::Request, msg.class)
+    assert_equal([0xF0, 0x41, 0x10, 0x42, 0x11, [0x40, 0x00, 0x7F], [0x00], 0x41, 0xF7], msg.to_a)
+    assert_equal([0xF0, 0x41, 0x10, 0x42, 0x11, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7], msg.to_bytes)
+    assert_equal("F04110421140007F0041F7", msg.to_hex_s)
+  end
+  
+  def test_system_exclusive_node
+    factory = MIDIMessageFactory.new
+    msg = factory.system_exclusive(0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7)
+    node = msg.node   
+    assert_equal(MIDIMessage::SystemExclusive::Node, node.class)
+    assert_equal(0x41, node.manufacturer_id)
+    assert_equal(0x42, node.model_id)
+    assert_equal(0x10, node.device_id)    
+  end
    
   def test_system_common_generic_3_bytes
     factory = MIDIMessageFactory.new
