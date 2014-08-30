c80eed0b2b24178a4758d37b2ec24b9fe13a01cc
Ari Russo <ari.russo@gmail.com>
2014-08-30 18:38:48 -0400

    extract methods, add comments

--- lib/nibbler/session.rb
+++ lib/nibbler/session.rb
@@ -19,77 +19,114 @@ module Nibbler
     def_delegator :clear_rejected, :rejected, :clear
     def_delegator :clear_messages, :messages, :clear
 
+    # @param [Hash] options
+    # @option options [Boolean] :timestamps Whether to report timestamps
     def initialize(options = {})
       @timestamps = options[:timestamps] || false
       @callbacks, @processed, @rejected, @messages = [], [], [], []
       @parser = Parser.new(options)    
     end
     
+    # @return [Array<Object>]
     def all_messages
       @messages | @fragmented_messages
     end
     
+    # The buffer as a single hex string
+    # @return [String]
     def buffer_s
       buffer.join
     end
     alias_method :buffer_hex, :buffer_s
 
+    # Clear the parser buffer
     def clear_buffer
       buffer.clear
     end
 
+    # Clear the message log
     def clear_messages
       @messages.clear
     end
     
     # Convert messages to hashes with timestamps
     def use_timestamps
-      @messages = @messages.map do |message|
-        { 
-          :messages => message, 
-          :timestamp => nil
-        }
+      if !@timestamps
+        @messages = @messages.map do |message|
+          { 
+            :messages => message, 
+            :timestamp => nil
+          }
+        end
+        @timestamps = true
       end
-      @timestamps = true
     end
 
+    # Parse some input
+    # @param [*Object] args
+    # @param [Hash] options (can be included as the last arg)
+    # @option options [Time] :timestamp A timestamp to store with the messages that result
+    # @return [Array<Object>, Hash]
     def parse(*args)
-      args.compact!
-      options = args.pop if args.last.kind_of?(Hash) 
+      options = args.last.kind_of?(Hash) ? args.pop : {}
+      timestamp = options[:timestamp]
 
-      timestamp = options[:timestamp] if !options.nil? && !options[:timestamp].nil?
-      use_timestamps if !timestamp.nil? && !@timestamps 
+      use_timestamps if !timestamp.nil? 
 
-      queue = HexProcessor.process(args)
-      result = @parser.process(queue)
-      report_message(result[:messages], :timestamp => timestamp)
-      @processed += result[:processed]
-      @rejected += result[:rejected]
-      get_parse_output(result[:messages], options)
+      result = process(args)
+      log(result, timestamp)  
     end    
     
     private
+
+    # Process the input
+    # @param [Array<Object>] input
+    # @return [Hash]
+    def process(input)
+      queue = HexProcessor.process(input)
+      @parser.process(queue)
+    end
+
+    # @param [Hash] parser_report
+    # @param [Time] timestamp
+    # @return [Array<Object>, Hash]
+    def log(parser_report, timestamp)
+      num = log_message(parser_report[:messages], :timestamp => timestamp)
+      @processed += parser_report[:processed]
+      @rejected += parser_report[:rejected]
+      get_output(num)
+    end
         
-    def report_message(message, options = {})
+    # @param [Array<Object>] messages The MIDI messages to log
+    # @return [Fixnum] The number of MIDI messages logged
+    def log_message(messages, options = {})
       if @timestamps
+        messages_for_log = messages.count == 1 ? messages.first : messages
         @messages << { 
-          :messages => message, 
+          :messages => messages_for_log, 
           :timestamp => options[:timestamp] 
         }     
       else
-        @messages += message
+        @messages += messages
       end
+      messages.count
     end
-    
-    def get_parse_output(messages, options = nil)
-      # return type
-      # 0 messages: nil
-      # 1 message: the message
-      # >1 message: an array of messages
-      # might make sense to make this an array no matter what...iii dunnoo
-      output = messages.length < 2 ? (messages.empty? ? nil : messages[0]) : messages
-      output = { :messages => output, :timestamp => options[:timestamp] } if @timestamps && !options.nil?
-      output      
+
+    # A report on the given number of most recent messages
+    #
+    # If timestamps are being used, will be a hash of messages and timestamp,
+    # otherwise just the messages
+    #
+    # The messages type will vary depending on the number of messages that were parsed:
+    # 0 messages: nil
+    # 1 message: the message
+    # >1 message: an array of messages
+    #
+    # @param [Fixnum] num The number of new messages to report
+    # @return [Array<Object>, Hash] 
+    def get_output(num)
+      messages = @messages.last(num)
+      messages.count < 2 ? messages.first : messages 
     end
     
   end
