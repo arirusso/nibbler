1a287551bc1173328d46517e8d840b9870062a79
Ari Russo <ari.russo@gmail.com>
2014-08-30 18:38:55 -0400

    clean up test

--- test/session_test.rb
+++ test/session_test.rb
@@ -3,113 +3,133 @@ require "helper"
 class Nibbler::SessionTest < Test::Unit::TestCase
     
   def test_varying_length_strings
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse("9", "04", "040")
-    assert_equal(MIDIMessage::NoteOn, msg.class)
+    session = Nibbler::Session.new
+    message = session.parse("9", "04", "040")
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::NoteOn, message.class)
   end
   
   def test_note_off
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0x80, 0x40, 0x40)
-    assert_equal(MIDIMessage::NoteOff, msg.class)
-    assert_equal(0, msg.channel)
-    assert_equal(0x40, msg.note)
-    assert_equal(0x40, msg.velocity)  
+    session = Nibbler::Session.new
+    message = session.parse(0x80, 0x40, 0x40)
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::NoteOff, message.class)
+    assert_equal(0, message.channel)
+    assert_equal(0x40, message.note)
+    assert_equal(0x40, message.velocity)  
   end
   
   def test_note_on
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0x90, 0x40, 0x40)
-    assert_equal(MIDIMessage::NoteOn, msg.class)
-    assert_equal(0, msg.channel)
-    assert_equal(0x40, msg.note)
-    assert_equal(0x40, msg.velocity)  
+    session = Nibbler::Session.new
+    message = session.parse(0x90, 0x40, 0x40)
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::NoteOn, message.class)
+    assert_equal(0, message.channel)
+    assert_equal(0x40, message.note)
+    assert_equal(0x40, message.velocity)  
   end  
   
   def test_timestamp
-    nibbler = Nibbler::Session.new
+    session = Nibbler::Session.new
     stamp = Time.now.to_i
-    outp = nibbler.parse(0x90, 0x40, 0x40, :timestamp => stamp)
-    msg = outp[:messages]
-    assert_equal(MIDIMessage::NoteOn, msg.class)
-    assert_equal(0, msg.channel)
-    assert_equal(0x40, msg.note)
-    assert_equal(0x40, msg.velocity)
-    assert_equal(stamp, outp[:timestamp])      
+    report = session.parse(0x90, 0x40, 0x40, :timestamp => stamp)
+    message = report[:messages]
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::NoteOn, message.class)
+    assert_equal(0, message.channel)
+    assert_equal(0x40, message.note)
+    assert_equal(0x40, message.velocity)
+    assert_equal(stamp, report[:timestamp])      
   end
   
   def test_polyphonic_aftertouch
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xA1, 0x40, 0x40)
-    assert_equal(MIDIMessage::PolyphonicAftertouch, msg.class)
-    assert_equal(1, msg.channel)
-    assert_equal(0x40, msg.note)
-    assert_equal(0x40, msg.value)      
+    session = Nibbler::Session.new
+    message = session.parse(0xA1, 0x40, 0x40)
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::PolyphonicAftertouch, message.class)
+    assert_equal(1, message.channel)
+    assert_equal(0x40, message.note)
+    assert_equal(0x40, message.value)      
   end  
   
   def test_control_change
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xB2, 0x20, 0x20)
-    assert_equal(MIDIMessage::ControlChange, msg.class)
-    assert_equal(msg.channel, 2)
-    assert_equal(0x20, msg.index)
-    assert_equal(0x20, msg.value)    
+    session = Nibbler::Session.new
+    message = session.parse(0xB2, 0x20, 0x20)
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::ControlChange, message.class)
+    assert_equal(message.channel, 2)
+    assert_equal(0x20, message.index)
+    assert_equal(0x20, message.value)    
   end
   
   def test_program_change
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xC3, 0x40)
-    assert_equal(MIDIMessage::ProgramChange, msg.class)
-    assert_equal(3, msg.channel)
-    assert_equal(0x40, msg.program)  
+    session = Nibbler::Session.new
+    message = session.parse(0xC3, 0x40)
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::ProgramChange, message.class)
+    assert_equal(3, message.channel)
+    assert_equal(0x40, message.program)  
   end
   
   def test_channel_aftertouch
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xD3, 0x50)
-    assert_equal(MIDIMessage::ChannelAftertouch, msg.class)
-    assert_equal(3, msg.channel)
-    assert_equal(0x50, msg.value)  
+    session = Nibbler::Session.new
+    message = session.parse(0xD3, 0x50)
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::ChannelAftertouch, message.class)
+    assert_equal(3, message.channel)
+    assert_equal(0x50, message.value)  
   end  
     
   def test_pitch_bend
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xE0, 0x20, 0x00) # center
-    assert_equal(MIDIMessage::PitchBend, msg.class)
-    assert_equal(0, msg.channel)
-    assert_equal(0x20, msg.low)
-    assert_equal(0x00, msg.high)  
+    session = Nibbler::Session.new
+    message = session.parse(0xE0, 0x20, 0x00) # center
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::PitchBend, message.class)
+    assert_equal(0, message.channel)
+    assert_equal(0x20, message.low)
+    assert_equal(0x00, message.high)  
   end   
    
   def test_system_common_generic_3_bytes
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xF1, 0x50, 0xA0)
-    assert_equal(MIDIMessage::SystemCommon, msg.class)
-    assert_equal(1, msg.status[1])
-    assert_equal(0x50, msg.data[0])
-    assert_equal(0xA0, msg.data[1])  
+    session = Nibbler::Session.new
+    message = session.parse(0xF1, 0x50, 0xA0)
+
+    assert_not_nil message
+    assert_equal(MIDIMessage::SystemCommon, message.class)
+    assert_equal(1, message.status[1])
+    assert_equal(0x50, message.data[0])
+    assert_equal(0xA0, message.data[1])  
   end    
 
   def test_system_common_generic_2_bytes
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xF1, 0x50)
-    assert_equal(MIDIMessage::SystemCommon, msg.class)
-    assert_equal(1, msg.status[1])
-    assert_equal(0x50, msg.data[0])  
+    session = Nibbler::Session.new
+    message = session.parse(0xF1, 0x50)
+    assert_equal(MIDIMessage::SystemCommon, message.class)
+    assert_equal(1, message.status[1])
+    assert_equal(0x50, message.data[0])  
   end    
 
   def test_system_common_generic_1_byte
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xF1)
-    assert_equal(MIDIMessage::SystemCommon, msg.class)
-    assert_equal(1, msg.status[1])
+    session = Nibbler::Session.new
+    message = session.parse(0xF1)
+    assert_equal(MIDIMessage::SystemCommon, message.class)
+    assert_equal(1, message.status[1])
   end    
   
   def test_system_realtime
-    nibbler = Nibbler::Session.new
-    msg = nibbler.parse(0xF8)
-    assert_equal(MIDIMessage::SystemRealtime, msg.class)
-    assert_equal(8, msg.id)
+    session = Nibbler::Session.new
+    message = session.parse(0xF8)
+    assert_equal(MIDIMessage::SystemRealtime, message.class)
+    assert_equal(8, message.id)
   end
                  
 end
