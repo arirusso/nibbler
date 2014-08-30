74b17663a54d3c7851e92fa3e732bf263a0f913d
Ari Russo <ari.russo@gmail.com>
2014-08-30 17:37:00 -0400

    name change

--- test/session_test.rb
+++ test/session_test.rb
@@ -1,15 +1,15 @@
 require "helper"
 
-class NibblerTest < Test::Unit::TestCase
+class Nibbler::SessionTest < Test::Unit::TestCase
     
   def test_varying_length_strings
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse("9", "04", "040")
     assert_equal(MIDIMessage::NoteOn, msg.class)
   end
   
   def test_note_off
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0x80, 0x40, 0x40)
     assert_equal(MIDIMessage::NoteOff, msg.class)
     assert_equal(0, msg.channel)
@@ -18,7 +18,7 @@ class NibblerTest < Test::Unit::TestCase
   end
   
   def test_note_on
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0x90, 0x40, 0x40)
     assert_equal(MIDIMessage::NoteOn, msg.class)
     assert_equal(0, msg.channel)
@@ -27,7 +27,7 @@ class NibblerTest < Test::Unit::TestCase
   end  
   
   def test_timestamp
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     stamp = Time.now.to_i
     outp = nibbler.parse(0x90, 0x40, 0x40, :timestamp => stamp)
     msg = outp[:messages]
@@ -39,7 +39,7 @@ class NibblerTest < Test::Unit::TestCase
   end
   
   def test_polyphonic_aftertouch
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xA1, 0x40, 0x40)
     assert_equal(MIDIMessage::PolyphonicAftertouch, msg.class)
     assert_equal(1, msg.channel)
@@ -48,7 +48,7 @@ class NibblerTest < Test::Unit::TestCase
   end  
   
   def test_control_change
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xB2, 0x20, 0x20)
     assert_equal(MIDIMessage::ControlChange, msg.class)
     assert_equal(msg.channel, 2)
@@ -57,7 +57,7 @@ class NibblerTest < Test::Unit::TestCase
   end
   
   def test_program_change
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xC3, 0x40)
     assert_equal(MIDIMessage::ProgramChange, msg.class)
     assert_equal(3, msg.channel)
@@ -65,7 +65,7 @@ class NibblerTest < Test::Unit::TestCase
   end
   
   def test_channel_aftertouch
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xD3, 0x50)
     assert_equal(MIDIMessage::ChannelAftertouch, msg.class)
     assert_equal(3, msg.channel)
@@ -73,7 +73,7 @@ class NibblerTest < Test::Unit::TestCase
   end  
     
   def test_pitch_bend
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xE0, 0x20, 0x00) # center
     assert_equal(MIDIMessage::PitchBend, msg.class)
     assert_equal(0, msg.channel)
@@ -82,7 +82,7 @@ class NibblerTest < Test::Unit::TestCase
   end   
    
   def test_system_common_generic_3_bytes
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xF1, 0x50, 0xA0)
     assert_equal(MIDIMessage::SystemCommon, msg.class)
     assert_equal(1, msg.status[1])
@@ -91,7 +91,7 @@ class NibblerTest < Test::Unit::TestCase
   end    
 
   def test_system_common_generic_2_bytes
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xF1, 0x50)
     assert_equal(MIDIMessage::SystemCommon, msg.class)
     assert_equal(1, msg.status[1])
@@ -99,14 +99,14 @@ class NibblerTest < Test::Unit::TestCase
   end    
 
   def test_system_common_generic_1_byte
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xF1)
     assert_equal(MIDIMessage::SystemCommon, msg.class)
     assert_equal(1, msg.status[1])
   end    
   
   def test_system_realtime
-    nibbler = Nibbler.new
+    nibbler = Nibbler::Session.new
     msg = nibbler.parse(0xF8)
     assert_equal(MIDIMessage::SystemRealtime, msg.class)
     assert_equal(8, msg.id)
