04af5625ad30204a7d53c6bf301e1b761f28b039
Ari Russo <ari.russo@gmail.com>
2014-08-30 16:03:51 -0400

    not needed

--- test/midilib_test.rb
+++ test/midilib_test.rb
@@ -1,14 +1,12 @@
 require "helper"
+require "nibbler/midilib"
 
 class MidilibTest < Test::Unit::TestCase
 
-  include Nibbler
   include TestHelper
-  
-  Parser.new(:message_lib => :midilib)
- 
+   
   def test_note_off
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.note_off(0x0, 0x40, 0x40)
     assert_equal(MIDI::NoteOff, message.class)
     assert_equal(0, message.channel)
@@ -17,7 +15,7 @@ class MidilibTest < Test::Unit::TestCase
   end
   
   def test_note_on
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.note_on(0x0, 0x40, 0x40)
     assert_equal(MIDI::NoteOn, message.class)
     assert_equal(0, message.channel)
@@ -26,7 +24,7 @@ class MidilibTest < Test::Unit::TestCase
   end  
   
   def test_polyphonic_aftertouch
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.polyphonic_aftertouch(0x1, 0x40, 0x40)
     assert_equal(MIDI::PolyPressure, message.class)
     assert_equal(1, message.channel)
@@ -35,7 +33,7 @@ class MidilibTest < Test::Unit::TestCase
   end  
   
   def test_control_change
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.control_change(0x2, 0x20, 0x20)
     assert_equal(MIDI::Controller, message.class)
     assert_equal(message.channel, 2)
@@ -44,7 +42,7 @@ class MidilibTest < Test::Unit::TestCase
   end
   
   def test_program_change
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.program_change(0x3, 0x40)
     assert_equal(MIDI::ProgramChange, message.class)
     assert_equal(3, message.channel)
@@ -52,7 +50,7 @@ class MidilibTest < Test::Unit::TestCase
   end
   
   def test_channel_aftertouch
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.channel_aftertouch(0x3, 0x50)
     assert_equal(MIDI::ChannelPressure, message.class)
     assert_equal(3, message.channel)
@@ -62,7 +60,7 @@ class MidilibTest < Test::Unit::TestCase
   def test_pitch_bend
     # to-do handle the midilib lsb/msb
     # right now the second data byte is being thrown away
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.pitch_bend(0x0, 0x20, 0x00)
     assert_equal(MIDI::PitchBend, message.class)
     assert_equal(0, message.channel)
@@ -70,64 +68,64 @@ class MidilibTest < Test::Unit::TestCase
   end
   
   def test_system_exclusive
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_exclusive(0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7)   
     assert_equal(MIDI::SystemExclusive, message.class)
     assert_equal([0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7], message.data)
   end
   
   def test_song_pointer
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_common(0x2, 0xF0)
     assert_equal(MIDI::SongPointer, message.class)
     assert_equal(0xF0, message.pointer)
   end
   
   def test_song_select
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_common(0x3, 0xA0)
     assert_equal(MIDI::SongSelect, message.class)
     assert_equal(0xA0, message.song)
   end
   
   def test_tune_request
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_common(0x6)
     assert_equal(MIDI::TuneRequest, message.class)
   end
   
   def test_clock
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_realtime(0x8)
     assert_equal(MIDI::Clock, message.class)
   end    
 
   def test_start
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_realtime(0xA)
     assert_equal(MIDI::Start, message.class)
   end     
   
   def test_continue
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_realtime(0xB)
     assert_equal(MIDI::Continue, message.class)
   end
   
   def test_stop
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_realtime(0xC)
     assert_equal(MIDI::Stop, message.class)
   end    
   
   def test_sense
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_realtime(0xE)
     assert_equal(MIDI::ActiveSense, message.class)
   end       
 
   def test_reset
-    lib = Midilib
+    lib = Nibbler::Midilib
     message = lib.system_realtime(0xF)
     assert_equal(MIDI::SystemReset, message.class)
   end       
