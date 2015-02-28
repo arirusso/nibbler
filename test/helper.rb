a727ad9abd06c8419a473d686ed3138a0d99894f
Ari Russo <ari.russo@gmail.com>
2015-02-28 10:10:09 -0500

    add mocha/shoulda

--- test/helper.rb
+++ test/helper.rb
@@ -2,4 +2,6 @@ dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift dir + "/../lib"
 
 require "minitest/autorun"
+require "mocha/test_unit"
+require "shoulda-context"
 require "nibbler"
