3ecd73dfb65e687891dcea3735e9dfdf2889bb68
Ari Russo <ari.russo@gmail.com>
2015-03-01 15:09:29 -0500

    better var name

--- lib/nibbler/parser.rb
+++ lib/nibbler/parser.rb
@@ -26,15 +26,15 @@ module Nibbler
       @buffer += nibbles
       # Iterate through nibbles in the buffer until a status message is found
       while pointer <= (@buffer.length - 1)
-        # current is the current piece of the buffer we're dealing with
-        current = get_current(pointer)
+        # fragment is the piece of the buffer to look at
+        fragment = get_fragment(pointer)
         # See if there really is a message there
-        unless (processed = nibbles_to_message(current)).nil?
-          # if current contains a real message, reject the nibbles that precede it
+        unless (processed = nibbles_to_message(fragment)).nil?
+          # if fragment contains a real message, reject the nibbles that precede it
           report[:rejected] += @buffer.slice(0, pointer)
           # and record it
-          @buffer = current.dup # Current now has the remaining nibbles for next pass
-          current = nil # Reset current
+          @buffer = fragment.dup # fragment now has the remaining nibbles for next pass
+          fragment = nil # Reset fragment
           pointer = 0 # Reset iterator
           report[:messages] << processed[:message]
           report[:processed] += processed[:processed]
@@ -47,10 +47,10 @@ module Nibbler
     end
 
     # @return [Hash, nil]
-    def nibbles_to_message(current)
-      if current.length >= 2
-        slice = current.slice(0..1).map(&:hex)
-        compute_message(slice, current)
+    def nibbles_to_message(fragment)
+      if fragment.length >= 2
+        slice = fragment.slice(0..1).map(&:hex)
+        compute_message(slice, fragment)
       end
     end
 
@@ -59,23 +59,23 @@ module Nibbler
     # Attempt to convert the given nibbles into a MIDI message
     # @param [Array<Fixnum>] nibbles
     # @return [Hash, nil]
-    def compute_message(nibbles, current)
+    def compute_message(nibbles, fragment)
       case nibbles[0]
-      when 0x8 then lookahead(6, current) { |status_2, bytes| @message.note_off(status_2, bytes[1], bytes[2]) }
-      when 0x9 then lookahead(6, current) { |status_2, bytes| @message.note_on(status_2, bytes[1], bytes[2]) }
-      when 0xA then lookahead(6, current) { |status_2, bytes| @message.polyphonic_aftertouch(status_2, bytes[1], bytes[2]) }
-      when 0xB then lookahead(6, current) { |status_2, bytes| @message.control_change(status_2, bytes[1], bytes[2]) }
-      when 0xC then lookahead(4, current) { |status_2, bytes| @message.program_change(status_2, bytes[1]) }
-      when 0xD then lookahead(4, current) { |status_2, bytes| @message.channel_aftertouch(status_2, bytes[1]) }
-      when 0xE then lookahead(6, current) { |status_2, bytes| @message.pitch_bend(status_2, bytes[1], bytes[2]) }
+      when 0x8 then lookahead(6, fragment) { |status_2, bytes| @message.note_off(status_2, bytes[1], bytes[2]) }
+      when 0x9 then lookahead(6, fragment) { |status_2, bytes| @message.note_on(status_2, bytes[1], bytes[2]) }
+      when 0xA then lookahead(6, fragment) { |status_2, bytes| @message.polyphonic_aftertouch(status_2, bytes[1], bytes[2]) }
+      when 0xB then lookahead(6, fragment) { |status_2, bytes| @message.control_change(status_2, bytes[1], bytes[2]) }
+      when 0xC then lookahead(4, fragment) { |status_2, bytes| @message.program_change(status_2, bytes[1]) }
+      when 0xD then lookahead(4, fragment) { |status_2, bytes| @message.channel_aftertouch(status_2, bytes[1]) }
+      when 0xE then lookahead(6, fragment) { |status_2, bytes| @message.pitch_bend(status_2, bytes[1], bytes[2]) }
       when 0xF then
         case nibbles[1]
-        when 0x0 then lookahead_sysex(current) { |bytes| @message.system_exclusive(*bytes) }
-        when 0x1..0x6 then lookahead(6, current, :recursive => true) { |status_2, bytes| @message.system_common(status_2, bytes[1], bytes[2]) }
-        when 0x8..0xF then lookahead(2, current) { |status_2, bytes| @message.system_realtime(status_2) }
+        when 0x0 then lookahead_sysex(fragment) { |bytes| @message.system_exclusive(*bytes) }
+        when 0x1..0x6 then lookahead(6, fragment, :recursive => true) { |status_2, bytes| @message.system_common(status_2, bytes[1], bytes[2]) }
+        when 0x8..0xF then lookahead(2, fragment) { |status_2, bytes| @message.system_realtime(status_2) }
         end
       else
-        use_running_status(current) if possible_running_status?
+        use_running_status(fragment) if possible_running_status?
       end
     end
 
@@ -98,26 +98,26 @@ module Nibbler
       !@running_status.nil?
     end
 
-    def use_running_status(current)
-      lookahead(@running_status[:num], current, :status_nibble => @running_status[:status_nibble], &@running_status[:callback])
+    def use_running_status(fragment)
+      lookahead(@running_status[:num], fragment, :status_nibble => @running_status[:status_nibble], &@running_status[:callback])
     end
 
-    def get_current(pointer)
+    def get_fragment(pointer)
       @buffer[pointer, (@buffer.length - pointer)]
     end
 
-    def lookahead(num, current, options = {}, &callback)
+    def lookahead(num, fragment, options = {}, &callback)
       # do we have enough nibbles for num bytes?
-      if current.size >= num
+      if fragment.size >= num
         # if so shift those nibbles off of the array and call block with them
-        nibbles = current.slice!(0, num)
+        nibbles = fragment.slice!(0, num)
         status_nibble ||= options[:status_nibble] || nibbles[1]
 
         # send the nibbles to the block as bytes
         # return the evaluated block and the remaining nibbles
         bytes = TypeConversion.hex_chars_to_numeric_bytes(nibbles)
 
-        # record the current situation in case running status comes up next round
+        # record the fragment situation in case running status comes up next round
         @running_status = {
           :callback => callback,
           :num => num - 2,
@@ -129,18 +129,18 @@ module Nibbler
           :processed => nibbles
         }
       elsif num > 0 && !!options[:recursive]
-        lookahead(num - 2, current, options, &callback)
+        lookahead(num - 2, fragment, options, &callback)
       end
     end
 
-    def lookahead_sysex(current, &block)
+    def lookahead_sysex(fragment, &block)
       @running_status = nil
 
-      bytes = TypeConversion.hex_chars_to_numeric_bytes(current)
+      bytes = TypeConversion.hex_chars_to_numeric_bytes(fragment)
       unless (index = bytes.index(0xF7)).nil?
         {
           :message => yield(bytes.slice!(0, index + 1)),
-          :processed => current.slice!(0, (index + 1) * 2)
+          :processed => fragment.slice!(0, (index + 1) * 2)
         }
       end
     end
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -6,8 +6,8 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     num = 6
     parser.instance_variable_set("@buffer", ["9", "0", "4", "0", "5", "0"])
-    current = parser.send(:get_current, 0)
-    output = parser.send(:lookahead, num, current) { |nibble_2, bytes| [nibble_2, bytes] }
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:lookahead, num, fragment) { |nibble_2, bytes| [nibble_2, bytes] }
     assert_equal([0,[0x90, 0x40, 0x50]], output[:message])
     assert_equal(["9", "0", "4", "0", "5", "0"], output[:processed])
   end
@@ -16,8 +16,8 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     num = 6
     parser.instance_variable_set("@buffer", ["9", "0", "4", "0", "5", "0", "5", "0"])
-    current = parser.send(:get_current, 0)
-    output = parser.send(:lookahead, num, current) { |nibble_2, bytes| [nibble_2, bytes] }
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:lookahead, num, fragment) { |nibble_2, bytes| [nibble_2, bytes] }
     assert_equal([0,[0x90, 0x40, 0x50]], output[:message])
     assert_equal(["9", "0", "4", "0", "5", "0"], output[:processed])
   end
@@ -26,8 +26,8 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     num = 6
     parser.instance_variable_set("@buffer", ["9", "0", "4"])
-    current = parser.send(:get_current, 0)
-    output = parser.send(:lookahead, num, current) do |nibble_2, bytes|
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:lookahead, num, fragment) do |nibble_2, bytes|
       {
         :message => nibble_2,
         :processed => bytes
@@ -40,8 +40,8 @@ class Nibbler::ParserTest < Minitest::Test
   def test_lookahead_sysex
     parser = Nibbler::Parser.new
     parser.instance_variable_set("@buffer", "F04110421240007F0041F750".split(//))
-    current = parser.send(:get_current, 0)
-    output = parser.send(:lookahead_sysex, current) { |b| b }
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:lookahead_sysex, fragment) { |b| b }
     assert_equal([0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7], output[:message])
     assert_equal("F04110421240007F0041F7".split(//), output[:processed])
   end
@@ -49,8 +49,8 @@ class Nibbler::ParserTest < Minitest::Test
   def test_lookahead_sysex_too_short
     parser = Nibbler::Parser.new
     parser.instance_variable_set("@buffer", ["9", "0", "4"])
-    current = parser.send(:get_current, 0)
-    output = parser.send(:lookahead_sysex, current) { |b| b }
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:lookahead_sysex, fragment) { |b| b }
 
     assert_nil output
   end
@@ -101,8 +101,8 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     short = ["5", "0", "9", "0", "4", "0", "5", "0"]
     parser.instance_variable_set("@buffer", short)
-    current = parser.send(:get_current, 0)
-    output = parser.send(:nibbles_to_message, current)
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:nibbles_to_message, fragment)
 
     assert_nil output
     assert_equal(["5", "0", "9", "0", "4", "0", "5", "0"], parser.buffer)
@@ -112,8 +112,8 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     short = ["9", "0", "4", "0", "5", "0", "5", "0"]
     parser.instance_variable_set("@buffer", short)
-    current = parser.send(:get_current, 0)
-    output = parser.send(:nibbles_to_message, current)
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:nibbles_to_message, fragment)
 
     refute_nil output
     assert_equal(::MIDIMessage::NoteOn, output[:message].class)
@@ -124,8 +124,8 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     short = ["9", "0", "4", "0", "5", "0", "5", "0"]
     parser.instance_variable_set("@buffer", short)
-    current = parser.send(:get_current, 0)
-    output = parser.send(:nibbles_to_message, current)
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:nibbles_to_message, fragment)
 
     refute_nil output
     assert_equal(::MIDIMessage::NoteOn, output[:message].class)
@@ -136,16 +136,16 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     short = ["9", "0", "4", "0", "5", "0"]
     parser.instance_variable_set("@buffer", short)
-    current = parser.send(:get_current, 0)
-    output = parser.send(:nibbles_to_message, current)
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:nibbles_to_message, fragment)
 
     refute_nil output
     assert_equal(::MIDIMessage::NoteOn, output[:message].class)
 
     running_status = ["5", "0", "6", "0"]
     parser.instance_variable_set("@buffer", running_status)
-    current = parser.send(:get_current, 0)
-    output = parser.send(:nibbles_to_message, current)
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:nibbles_to_message, fragment)
 
     refute_nil output
     assert_equal(::MIDIMessage::NoteOn, output[:message].class)
@@ -156,8 +156,8 @@ class Nibbler::ParserTest < Minitest::Test
     parser = Nibbler::Parser.new
     sysex = "F04110421240007F0041F750".split(//)
     parser.instance_variable_set("@buffer", sysex)
-    current = parser.send(:get_current, 0)
-    output = parser.send(:nibbles_to_message, current)
+    fragment = parser.send(:get_fragment, 0)
+    output = parser.send(:nibbles_to_message, fragment)
 
     refute_nil output
     assert_equal(::MIDIMessage::SystemExclusive::Command, output[:message].class)
