48a8e907f7658c0fea155ba147c2deccad1fe8db
Ari Russo <ari.russo@gmail.com>
2015-03-01 14:42:34 -0500

    add comments

--- lib/nibbler/parser.rb
+++ lib/nibbler/parser.rb
@@ -5,7 +5,7 @@ module Nibbler
     attr_reader :buffer
 
     # @param [Hash] options
-    # @option options [Symbol] :message_lib
+    # @option options [Symbol] :message_lib The name of a message library module eg MIDIMessage or Midilib
     def initialize(options = {})
       @running_status = nil
       @buffer = []
@@ -14,34 +14,35 @@ module Nibbler
       initialize_message_library(options[:message_lib])
     end
 
+    # Process the given nibbles and add them to the buffer
     # @param [Array<String, Fixnum>] nibbles
     # @return [Hash]
     def process(nibbles)
-      report = { 
-        :messages => [], 
-        :processed => [], 
-        :rejected => [] 
-      }          
-      @iterator = 0      
-      @buffer += nibbles  
+      report = {
+        :messages => [],
+        :processed => [],
+        :rejected => []
+      }
+      @iterator = 0
+      @buffer += nibbles
+      # Iterate through nibbles in the buffer until a status message is found
       while @iterator <= (@buffer.length - 1)
-        # iterate through nibbles until a status message is found        
-        # see if there really is a message there
+        # current is the current piece of the buffer we're dealing with
         populate_current
-        # current is the current piece of the buffer we"re dealing with
+        # See if there really is a message there
         unless (processed = nibbles_to_message).nil?
-          # if it"s a real message, reject previous nibbles
-          report[:rejected] += @buffer.slice(0, @iterator)          
-          # and record it          
-          @buffer = @current # current now has the remaining nibbles for next pass
-          @current = nil # reset current
-          @iterator = 0 # reset iterator
+          # if current contains a real message, reject the nibbles that precede it
+          report[:rejected] += @buffer.slice(0, @iterator)
+          # and record it
+          @buffer = @current # Current now has the remaining nibbles for next pass
+          @current = nil # Reset current
+          @iterator = 0 # Reset iterator
           report[:messages] << processed[:message]
-          report[:processed] += processed[:processed]          
+          report[:processed] += processed[:processed]
         else
           @running_status = nil
           @iterator += 1
-        end         
+        end
       end
       report
     end
@@ -50,12 +51,13 @@ module Nibbler
     def nibbles_to_message
       if @current.length >= 2
         nibbles = @current.slice(0..1).map(&:hex)
-        compute_message(nibbles)           
+        compute_message(nibbles)
       end
     end
 
     private
 
+    # Attempt to convert the given nibbles into a MIDI message
     # @param [Array<Fixnum>] nibbles
     # @return [Hash, nil]
     def compute_message(nibbles)
@@ -67,36 +69,39 @@ module Nibbler
       when 0xC then lookahead(4) { |status_2, bytes| @message.program_change(status_2, bytes[1]) }
       when 0xD then lookahead(4) { |status_2, bytes| @message.channel_aftertouch(status_2, bytes[1]) }
       when 0xE then lookahead(6) { |status_2, bytes| @message.pitch_bend(status_2, bytes[1], bytes[2]) }
-      when 0xF then 
+      when 0xF then
         case nibbles[1]
         when 0x0 then lookahead_sysex { |bytes| @message.system_exclusive(*bytes) }
         when 0x1..0x6 then lookahead(6, :recursive => true) { |status_2, bytes| @message.system_common(status_2, bytes[1], bytes[2]) }
         when 0x8..0xF then lookahead(2) { |status_2, bytes| @message.system_realtime(status_2) }
         end
       else
-        use_running_status if possible_running_status? 
+        use_running_status if possible_running_status?
       end
     end
 
     # Choose a MIDI message object library
+    # @param [Symbol] lib The MIDI message library module eg MIDIMessage or Midilib
+    # @return [Module]
     def initialize_message_library(lib)
       @message = case lib
-      when :midilib then    
+      when :midilib then
         require "nibbler/midilib"
         ::Nibbler::Midilib
-      else      
-        require "nibbler/midi-message"    
-        ::Nibbler::MIDIMessage    
+      else
+        require "nibbler/midi-message"
+        ::Nibbler::MIDIMessage
       end
     end
 
+    # @return [Boolean]
     def possible_running_status?
       !@running_status.nil?
     end
 
     def use_running_status
       lookahead(@running_status[:num], :status_nibble => @running_status[:status_nibble], &@running_status[:callback])
-    end  
+    end
 
     def populate_current
       @current = (@buffer[@iterator, (@buffer.length - @iterator)])
@@ -107,17 +112,17 @@ module Nibbler
       if @current.size >= num
         # if so shift those nibbles off of the array and call block with them
         nibbles = @current.slice!(0, num)
-        status_nibble ||= options[:status_nibble] || nibbles[1]  
+        status_nibble ||= options[:status_nibble] || nibbles[1]
 
-        # send the nibbles to the block as bytes         
-        # return the evaluated block and the remaining nibbles       
+        # send the nibbles to the block as bytes
+        # return the evaluated block and the remaining nibbles
         bytes = TypeConversion.hex_chars_to_numeric_bytes(nibbles)
 
         # record the current situation in case running status comes up next round
-        @running_status = { 
-          :callback => callback, 
-          :num => num - 2, 
-          :status_nibble => status_nibble 
+        @running_status = {
+          :callback => callback,
+          :num => num - 2,
+          :status_nibble => status_nibble
         }
 
         {
