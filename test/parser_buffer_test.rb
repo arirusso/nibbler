19d8691d0f8eba89ecb75e45cffa3b228744be57
Ari Russo <ari.russo@gmail.com>
2015-03-01 12:05:24 -0500

    use shoulda

--- test/parser_buffer_test.rb
+++ test/parser_buffer_test.rb
@@ -2,65 +2,63 @@ require "helper"
 
 class Nibbler::BufferTest < Minitest::Test
 
-  def test_buffer_nibble_str
-    nibbler = Nibbler.new
-    nibbler.parse("9")
-    assert_equal(["9"], nibbler.buffer)
-  end
+  context "Parser::Buffer" do
 
-  def test_buffer_single_byte
-    nibbler = Nibbler.new
-    nibbler.parse(0x90)
-    assert_equal(["9", "0"], nibbler.buffer)
-  end
+    setup do
+      @nibbler = Nibbler.new
+    end
 
-  def test_buffer_single_byte_str
-    nibbler = Nibbler.new
-    nibbler.parse("90")
-    assert_equal(["9", "0"], nibbler.buffer)
-  end
+    should "have processed string nibble" do
+      @nibbler.parse("9")
+      assert_equal(["9"], @nibbler.buffer)
+    end
 
-  def test_buffer_single_byte_in_array
-    nibbler = Nibbler.new
-    nibbler.parse([0x90])
-    assert_equal(["9", "0"], nibbler.buffer)
-  end
+    should "have processed numeric byte" do
+      @nibbler.parse(0x90)
+      assert_equal(["9", "0"], @nibbler.buffer)
+    end
 
-  def test_buffer_two_bytes
-    nibbler = Nibbler.new
-    nibbler.parse(0x90, 0x40)
-    assert_equal(["9", "0", "4", "0"], nibbler.buffer)
-  end
+    should "have processed string byte" do
+      @nibbler.parse("90")
+      assert_equal(["9", "0"], @nibbler.buffer)
+    end
 
-  def test_buffer_two_bytes_str
-    nibbler = Nibbler.new
-    nibbler.parse("90", "40")
-    assert_equal(["9", "0", "4", "0"], nibbler.buffer)
-  end
+    should "have processed array" do
+      @nibbler.parse([0x90])
+      assert_equal(["9", "0"], @nibbler.buffer)
+    end
 
-  def test_buffer_two_bytes_single_str
-    nibbler = Nibbler.new
-    nibbler.parse("9040")
-    assert_equal(["9", "0", "4", "0"], nibbler.buffer)
-  end
+    should "have processed numeric bytes" do
+      @nibbler.parse(0x90, 0x40)
+      assert_equal(["9", "0", "4", "0"], @nibbler.buffer)
+    end
 
-  def test_buffer_two_bytes_mixed
-    nibbler = Nibbler.new
-    nibbler.parse("90", 0x40)
-    assert_equal(["9", "0", "4", "0"], nibbler.buffer)
-  end
+    should "have processed string bytes" do
+      @nibbler.parse("90", "40")
+      assert_equal(["9", "0", "4", "0"], @nibbler.buffer)
+    end
 
-  def test_buffer_nibble_and_byte_mixed
-    nibbler = Nibbler.new
-    nibbler.parse("9", 0x40)
-    assert_equal(["9", "4", "0"], nibbler.buffer)
-  end
+    should "have processed two-byte string" do
+      @nibbler.parse("9040")
+      assert_equal(["9", "0", "4", "0"], @nibbler.buffer)
+    end
 
-  def test_buffer_stepped
-    nibbler = Nibbler.new
-    nibbler.parse("9")
-    nibbler.parse(0x40)
-    assert_equal(["9", "4", "0"], nibbler.buffer)
+    should "have processed mixed bytes" do
+      @nibbler.parse("90", 0x40)
+      assert_equal(["9", "0", "4", "0"], @nibbler.buffer)
+    end
+
+    should "have processed mixed nibble and byte" do
+      @nibbler.parse("9", 0x40)
+      assert_equal(["9", "4", "0"], @nibbler.buffer)
+    end
+
+    should "have processed separate data" do
+      @nibbler.parse("9")
+      @nibbler.parse(0x40)
+      assert_equal(["9", "4", "0"], @nibbler.buffer)
+    end
+
   end
 
 end
