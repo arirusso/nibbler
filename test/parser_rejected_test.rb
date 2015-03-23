95dc49785d8fa7e308be6b04337c3437844bf651
Ari Russo <ari.russo@gmail.com>
2015-03-22 20:55:49 -0400

    use shoulda

--- test/parser_rejected_test.rb
+++ test/parser_rejected_test.rb
@@ -2,59 +2,153 @@ require "helper"
 
 class Nibbler::ParserRejectedTest < Minitest::Test
 
-  def test_leading_chars
-    nibbler = Nibbler.new
-    msg = nibbler.parse("0", "9", "04", "040")
-    assert_equal(::MIDIMessage::NoteOn, msg.class)
-    assert_equal(["0"], nibbler.rejected)
-  end
+  context "Rejected" do
 
-  def test_2_leading_chars
-    nibbler = Nibbler.new
-    msg = nibbler.parse("1", "0", "9", "04", "040")
-    assert_equal(["1", "0"], nibbler.rejected)
-  end
+    setup do
+      @nibbler = Nibbler.new
+    end
 
-  def test_leading_string
-    nibbler = Nibbler.new
-    msg = nibbler.parse("10", "9", "04", "040")
-    assert_equal(::MIDIMessage::NoteOn, msg.class)
-    assert_equal(["1", "0"], nibbler.rejected)
-  end
+    context "leading chars" do
 
-  def test_long_leading_string
-    nibbler = Nibbler.new
-    msg = nibbler.parse("000001000010", "9", "04", "040")
-    assert_equal(::MIDIMessage::NoteOn, msg.class)
-    assert_equal("000001000010".split(//), nibbler.rejected)
-  end
+      setup do
+        @message = @nibbler.parse("0", "9", "04", "040")
+      end
 
-  def test_long_leading_string_overlap
-    nibbler = Nibbler.new
-    msg = nibbler.parse("000001000010090", "4", "040")
-    assert_equal(::MIDIMessage::NoteOn, msg.class)
-    assert_equal("0000010000100".split(//), nibbler.rejected)
-  end
+      should "return correct message" do
+        assert_equal(::MIDIMessage::NoteOn, @message.class)
+      end
 
-  def test_leading_number
-    nibbler = Nibbler.new
-    msg = nibbler.parse(0x30, "9", "04", "040")
-    assert_equal(::MIDIMessage::NoteOn, msg.class)
-    assert_equal(["3", "0"], nibbler.rejected)
-  end
+      should "reject extra char" do
+        refute_empty @nibbler.rejected
+        assert_equal("0", @nibbler.rejected.first)
+      end
 
-  def test_2_leading_numbers
-    nibbler = Nibbler.new
-    msg = nibbler.parse(0x60, 0x30, "9", "04", "040")
-    assert_equal(::MIDIMessage::NoteOn, msg.class)
-    assert_equal(["6", "0", "3", "0"], nibbler.rejected)
-  end
+    end
 
-  def test_3_leading_numbers
-    nibbler = Nibbler.new
-    msg = nibbler.parse(0x00, 0x30, "9", "04", "040")
-    assert_equal(::MIDIMessage::NoteOn, msg.class)
-    assert_equal(["0", "0", "3", "0"], nibbler.rejected)
+    context "2 leading chars" do
+
+      setup do
+        @message = @nibbler.parse("1", "0", "9", "04", "040")
+      end
+
+      should "reject two leading chars" do
+        refute_empty @nibbler.rejected
+        assert_equal "1", @nibbler.rejected[0]
+        assert_equal "0", @nibbler.rejected[1]
+      end
+
+    end
+
+    context "leading string" do
+
+      setup do
+        @message = @nibbler.parse("10", "9", "04", "040")
+      end
+
+      should "return correct message" do
+        assert_equal(::MIDIMessage::NoteOn, @message.class)
+      end
+
+      should "reject chars in leading string" do
+        refute_empty @nibbler.rejected
+        assert_equal "1", @nibbler.rejected[0]
+        assert_equal "0", @nibbler.rejected[1]
+      end
+
+    end
+
+    context "long leading string" do
+
+      setup do
+        @message = @nibbler.parse("000001000010", "9", "04", "040")
+      end
+
+      should "return correct message" do
+        assert_equal(::MIDIMessage::NoteOn, @message.class)
+      end
+
+      should "return string" do
+        refute_empty @nibbler.rejected
+        assert_equal("000001000010".split(//), @nibbler.rejected)
+      end
+
+    end
+
+    context "long leading string overlap" do
+
+      setup do
+        @message = @nibbler.parse("000001000010090", "4", "040")
+      end
+
+      should "return correct message" do
+        assert_equal(::MIDIMessage::NoteOn, @message.class)
+      end
+
+      should "return leading string" do
+        refute_empty @nibbler.rejected
+        assert_equal("0000010000100".split(//), @nibbler.rejected)
+      end
+
+    end
+
+    context "leading number" do
+
+      setup do
+        @message = @nibbler.parse(0x30, "9", "04", "040")
+      end
+
+      should "return correct message" do
+        assert_equal(::MIDIMessage::NoteOn, @message.class)
+      end
+
+      should "return leading numbers" do
+        refute_empty @nibbler.rejected
+        assert_equal "3", @nibbler.rejected[0]
+        assert_equal "0", @nibbler.rejected[1]
+      end
+
+    end
+
+    context "2 leading numbers" do
+
+      setup do
+        @message = @nibbler.parse(0x60, 0x30, "9", "04", "040")
+      end
+
+      should "return correct message" do
+        assert_equal(::MIDIMessage::NoteOn, @message.class)
+      end
+
+      should "return leading numbers" do
+        refute_empty @nibbler.rejected
+        assert_equal "6", @nibbler.rejected[0]
+        assert_equal "0", @nibbler.rejected[1]
+        assert_equal "3", @nibbler.rejected[2]
+        assert_equal "0", @nibbler.rejected[3]
+      end
+
+    end
+
+    context "3 leading numbers" do
+
+      setup do
+        @message = @nibbler.parse(0x00, 0x30, "9", "04", "040")
+      end
+
+      should "return correct message" do
+        assert_equal(::MIDIMessage::NoteOn, @message.class)
+      end
+
+      should "return leading numbers" do
+        refute_empty @nibbler.rejected
+        assert_equal "0", @nibbler.rejected[0]
+        assert_equal "0", @nibbler.rejected[1]
+        assert_equal "3", @nibbler.rejected[2]
+        assert_equal "0", @nibbler.rejected[3]
+      end
+
+    end
+
   end
 
 end
