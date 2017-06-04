3c41001a475ed822cf7c22bdd6127ae8f00ec7e7
Ari Russo <ari.russo@gmail.com>
2017-06-04 14:27:12 -0400

    formatting

--- examples/usage.rb
+++ examples/usage.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
-#
-# Walk through of different ways to use Nibbler
-#
+$:.unshift(File.join("..", "lib"))
 
-dir = File.dirname(File.expand_path(__FILE__))
-$LOAD_PATH.unshift dir + "/../lib"
+#
+# Walk through different ways to use Nibbler
+#
 
 require "nibbler"
 require "pp"
@@ -32,13 +31,13 @@ pp "Use nibbles in string format"
 pp nibbler.parse("9", "0", 0x40, 0x40) # this should return a message
 
 pp "Interchange the different types"
-  
+
 pp nibbler.parse("9", "0", 0x40, 64)
-  
+
 pp "Use running status"
 
 pp nibbler.parse(0x40, 64)
-  
+
 pp "Look at the messages we"ve parsed"
 
 pp nibbler.messages # this should return an array of messages
@@ -57,7 +56,7 @@ pp nibbler.buffer_s # should give you an array of bytestrs
 pp "Pass in a timestamp"
 
 # note:
-# once you pass in a timestamp for the first time, nibbler.messages will then return 
+# once you pass in a timestamp for the first time, nibbler.messages will then return
 # an array of message/timestamp hashes
 # if there was no timestamp for a particular message it will be nil
 #
@@ -66,18 +65,18 @@ pp nibbler.parse("904040", :timestamp => Time.now.to_i)
 
 pp "Add callbacks"
 
-# you can list any properties of the message to check against. 
+# you can list any properties of the message to check against.
 # if they are all true, the callback will fire
 #
 # if you wish to use "or" or any more advanced matching I would just process the message after it"s
 # returned
 #
-nibbler.when({ :class => MIDIMessage::NoteOn }) { |msg| puts "bark" } 
+nibbler.when({ :class => MIDIMessage::NoteOn }) { |msg| puts "bark" }
 pp nibbler.parse("904040")
 pp nibbler.parse("804040")
 
 pp "Generate midilib messages"
 
 midilib_nibbler = Nibbler.new(:message_lib => :midilib)
-  
+
 pp midilib_nibbler.parse("9", "0", 0x40, "40")
